0d26c9f8df1beaf3b9089c6a0a004fb57488d45c
Nagai Asuka <87313557+nagaiasuka@users.noreply.github.com>
2022-04-11 20:35:23 +0900

    コントローラー（画面表示）

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,10 @@ from .import views
 
 urlpatterns =[
     path('',views.index,name='index'),
+    path('create',views.create,name='create'),
+    path('store',views.store,name='store'),
+    path('show/<int:id>',views.show,name='show'),
+    path('<int:id>/edit',views.edit,name='edit'),
+    path('<int:id>/update',views.update,name='update'),
+    path('<int:id>/delete',views.destoroy,name='delete')
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,27 @@ from django.shortcuts import render
 # 一覧表示
 def index(request):
     return render(request, 'blog/index.html')
+
+# 新規投稿
+def create(request):
+    return render(request, 'blog/create.html')
+
+# 登録処理
+def store(request):
+    pass
+
+# 詳細画面
+def show(request):
+    return render(request, 'blog/show.html')
+
+# 編集更新画面
+def edit(request):
+    return render(request, 'blog/edit.html')
+
+# 更新処理
+def update(request):
+    pass
+
+# 削除
+def delete(request):
+    pass
