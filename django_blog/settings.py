565a1f2e3c964672c85c2653014425ce8237e812
Nagai Asuka <87313557+nagaiasuka@users.noreply.github.com>
2022-04-16 16:01:06 +0900

    ログイン・ログアウト機能実装

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'django_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,3 +130,6 @@ STATICFILES_DIRS = [BASE_DIR,"static"]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# ユーザー管理
+LOGIN_REDIRECT_URL = '/blog'
