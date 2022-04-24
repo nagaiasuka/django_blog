368863cbc7cad17b778245988499cd9e1daf684a
Nagai Asuka <87313557+nagaiasuka@users.noreply.github.com>
2022-04-24 20:33:00 +0900

    見た目修正

--- blog/forms.py
+++ blog/forms.py
@@ -4,4 +4,14 @@ from .models import Comment
 class CommentCreateForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ("text",)
+        fields = ('text'),
+        labels = {'text':'コメント',}
+        widgets = {
+            'text': forms.Textarea(
+                attrs={
+                    'class' : 'form-control',
+                    'placeholder':"内容",
+                    'rows': "5",
+                }
+            ),
+        }
